fba18682cf02ba66ce3873415849ae0a1e39378b
dilmirangana09 <dilmi.rangana09@gmail.com>
2026-03-26 23:19:20 +0530

    session.py

--- app/db/session.py
+++ app/db/session.py
@@ -57,5 +57,18 @@ engine = create_engine(
     pool_pre_ping=True,
 )
 
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine,
+)
+
 Base = declarative_base()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
